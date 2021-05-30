eb50498bd7f98f3fd39e1835abc5e803a3baa226
abdullah_mujeeb <kingdomofamk@gmail.com>
2021-05-30 20:22:59 +0500

    Changed data: open is now target with shift+1 of previous target

--- LR.py
+++ LR.py
@@ -4,14 +4,15 @@ from sklearn.model_selection import train_test_split
 import seaborn as sns
 from sklearn import metrics
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 from pycaret.classification import setup
 from pycaret.classification import compare_models
 
-df=pd.read_csv("btc_bars_6h.csv")
-df1=pd.read_csv('btc_bars_6h.csv', usecols=["newvalue"])
+df=pd.read_csv("btc_bars_4h.csv")
+#df1=pd.read_csv('btc_bars_1d.csv', usecols=["close"])
 df = df.loc[:, df.columns != 'newvalue']
+#df = df.loc[:, df.columns != 'close']
 df['date'] = pd.to_datetime(df['date'], errors='coerce', format='%Y-%m-%d').dt.strftime("%Y%m%d")
 #df['date'] = df['date'].str.replace('-','')
 #df['date'] = df['date'].str.replace(':','')
@@ -25,11 +26,28 @@ df['date'] = pd.to_datetime(df['date'], errors='coerce', format='%Y-%m-%d').dt.s
 #df['date'] = df['date'].str.replace(' 20','')
 #df['date'] = df['date'].str.replace(' 08','')
 
+df1=pd.read_csv('btc_bars_4h.csv', usecols=["open","newvalue"])
+df1['testingvalue'] = df1.newvalue.shift(+1)
+df1 = df1.iloc[1:-1]
+df = df.iloc[1:-1]
+df1['open'] = np.where(df1['open'], df1['testingvalue'], df1['open'])
 #df['date'] = df['date'][:-11]
+del df['open']
+del df1['newvalue']
+del df1['testingvalue']
 df.fillna(0, inplace=True)
+print(df.shape)
+print(df1.shape)
 print(df.head())
+print(df1.head())
+
+#print(df.head())
+#df= df.iloc[1:-1]
+#df1.newvalue = df1.newvalue.shift(+1)
+#df1 = df1.iloc[1:-1]
+
 x_train, x_test, y_train, y_test = train_test_split(df, df1, test_size=0.4, random_state=0)
-LR1 = LogisticRegression(solver='newton-cg', max_iter=15, tol=0.0001)
+LR1 = LogisticRegression(solver='newton-cg', max_iter=40, tol=0.0001)
 
 LR1.fit(x_train, y_train)
 predictions = LR1.predict(x_test)
@@ -51,16 +69,17 @@ print("precision: ", round(Precision*100), "%")
 print("recall :", round(Recall*100), "%")
 print("F1 Score: ", F1)
 
+#df123=df=pd.read_csv("btc_bars_1d.csv")
+#grid = setup(data=df, target=df1, html=False, silent=True, verbose=False)
+#best = compare_models()
+#print(best)
 
-grid = setup(data=df, target=df1, html=False, silent=True, verbose=False)
-best = compare_models()
-print(best)
 
 
 
-plt.figure(figsize=(9,9))
-sns.heatmap(cm, annot=True, fmt=".3f", linewidths=.5, square = True, cmap = 'Blues_r')
-plt.ylabel('Actual label')
-plt.xlabel('Predicted label')
-all_sample_title = 'Accuracy Score: {0}'.format(score)
-plt.title(all_sample_title, size = 15)
+#plt.figure(figsize=(9,9))
+#sns.heatmap(cm, annot=True, fmt=".3f", linewidths=.5, square = True, cmap = 'Blues_r')
+#plt.ylabel('Actual label')
+#plt.xlabel('Predicted label')
+#all_sample_title = 'Accuracy Score: {0}'.format(score)
+#plt.title(all_sample_title, size = 15)
